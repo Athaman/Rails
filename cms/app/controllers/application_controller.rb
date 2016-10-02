b0a7834a0c88af1ae2bbc83fd03e1a2f88e91aba
Athaman <keonemartin@gmail.com>
2016-10-02 21:24:16 +0200

    fixed variable clash name with active record in the cms

--- cms/app/controllers/application_controller.rb
+++ cms/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  before_filter :getPageNav, :getJumbotron, :getBlocks
+  before_filter :getPageNav, :getJumbotron,  :get_blocks
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -13,7 +13,8 @@ class ApplicationController < ActionController::Base
     @jumbotron = Block.where("position = 'jumbotron'")
   end
 
-  def getBlocks
-    @blocks = Block.where("position = 'block'").order(order: :asc)
+  def get_blocks
+    # note of interest, calling this variable block causes an active admin clash that crashes the block page in the admin area.
+    @the_blocks = Block.where("position = 'block'").order(order: :asc)
   end
 end
